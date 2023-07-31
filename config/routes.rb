2a12f0bdae83b00da6638f4e7bb61de0e71b74c0
Inazumi Satonao <fnherstal.five.seven@gmail.com>
2023-07-31 14:46:13 +0000

    2.7 修正

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   get 'posts/new', to: 'posts#new', as: 'new_post'
+  get 'posts/index', to: 'posts#index'
   get 'topics/new', to: 'topics#new', as: 'new_topic'
   get 'topics/edit', to: 'topics#edit', as: 'edit_topic' 
 end
